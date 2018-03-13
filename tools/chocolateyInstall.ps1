2b4016eb671a9823611b00261910db785d54083c
Alan Płócieniak <alan-null@outlook.com>
2018-03-13 21:00:05 +0100

    Initial commit

---
+++ tools/chocolateyInstall.ps1
@@ -0,0 +1,4 @@
+﻿$packageName = 'scla'
+$url = 'https://marketplace.sitecore.net/services/~/media/Marketplace/Modules/S/Sitecore_Log_Analyzer/packages/SCLA_2,-d-,0,-d-,0_rev,-d-,_140603.ashx'
+
+Install-ChocolateyZipPackage $packageName $url "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
